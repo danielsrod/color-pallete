1668920c6a4d0bc31a621d5ea0bc2e8946e8ab41
Daniel Rodrigues <coeng.daniel@gmail.com>
2022-11-21 15:43:32 -0400

    clipboard feature added | text color contrast added | icon clip board added | toast notification added

--- src/components/ColorPallete.vue
+++ src/components/ColorPallete.vue
@@ -1,12 +1,47 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
+import { createToaster } from '@meforma/vue-toaster';
+
+const toaster = createToaster({
+  maxToasts: 3,
+  duration: 2000
+});
+
 const colors = ref(['', '', '', '', '']);
 
 const generateRandomHex = () => {
   colors.value = colors.value.map((key) => {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return (
+      '#' + Math.floor(Math.random() * 16777215).toString(16)
+    ).toUpperCase();
   });
+};
+
+const copyClipBoard = async (color) => {
+  try {
+    await navigator.clipboard.writeText(color);
+    toaster.show('Copied to clipboard!', {
+      position: 'top-right',
+    });
+  } catch (err) {
+    toaster.show('Something went wrong!', {
+      position: 'top-right',
+    });
+  }
+};
+
+const getContrast = (hexcolor) => {
+  if (hexcolor.slice(0, 1) === '#') {
+    hexcolor = hexcolor.slice(1);
+  }
+
+  const r = parseInt(hexcolor.substr(0, 2), 16);
+  const g = parseInt(hexcolor.substr(2, 2), 16);
+  const b = parseInt(hexcolor.substr(4, 2), 16);
+
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? 'black' : 'white';
 };
 
 onMounted(() => {
@@ -20,18 +55,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container" @keypress="generateRandomHex()">
     <div class="header">
       <h1>Color Pallete Generator</h1>
     </div>
     <div class="colors-grid">
       <div
         class="color-box"
-        :style="{ background: color }"
-        v-for="(color, idx) in colors"
+        :style="{ background: colorHex }"
+        v-for="(colorHex, idx) in colors"
         :key="idx"
+        @click="copyClipBoard(colorHex)"
       >
-        {{ color }}
+        <span :style="{ color: getContrast(colorHex) }">{{ colorHex }}</span>
       </div>
     </div>
   </div>
@@ -51,6 +87,7 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 1.8rem;
 }
 
 .container {
@@ -72,5 +109,34 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   font-weight: bold;
+  cursor: default;
+  font-size: 1.8rem;
+  user-select: none;
+  color: white;
+}
+
+.color-box:hover:before {
+  animation: animation 0.15s ease;
+  position: absolute;
+  top: 55%;
+  content: url(https://api.iconify.design/iconoir:clipboard-check.svg?color=%23888888&height=30);
+}
+
+@keyframes animation {
+  from {
+    transform: scale(0);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+.do-contrast {
+  color: color-contrast(black vs white, black);
+  width: 100px;
+  height: 100px;
+  background: black;
 }
 </style>

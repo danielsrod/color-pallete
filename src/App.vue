0cb6f7cb404a89317387b549d3f1a1397f750fca
Daniel Rodrigues <coeng.daniel@gmail.com>
2022-11-20 23:46:12 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineComponent } from '@vue/runtime-core';
+import ColorPallete from './components/ColorPallete.vue';
+
+defineComponent({
+  ColorPallete,
+});
+</script>
+
+<template>
+  <ColorPallete></ColorPallete>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/ColorPallete.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const colors = ref(['', '', '', '', '']);
+
+const generateRandomHex = () => {
+  colors.value = colors.value.map((key) => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  });
+};
+
+onMounted(() => {
+  generateRandomHex();
+  addEventListener('keypress', function (e) {
+    if (e.code === 'Space') {
+      generateRandomHex();
+    }
+  });
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="header">
+      <h1>Color Pallete Generator</h1>
+    </div>
+    <div class="colors-grid">
+      <div
+        class="color-box"
+        :style="{ background: color }"
+        v-for="(color, idx) in colors"
+        :key="idx"
+      >
+        {{ color }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.header {
+  width: 100%;
+  height: 10vh;
+  background: rgb(62, 62, 62);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.colors-grid {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  justify-content: space-between;
+}
+
+.color-box {
+  width: 25%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+}
+</style>
